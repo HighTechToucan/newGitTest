62deaa19f2ee93ee5a9985fdbe5ee25d813879c9
ts2995 <ts2995@gmail.com>
2020-02-07 12:50:36 +0000

    Update Images.py

--- Images.py
+++ Images.py
@@ -18,55 +18,87 @@ my_img2 = ImageTk.PhotoImage(Image.open("images/test2.png"))
 my_img3 = ImageTk.PhotoImage(Image.open("images/test3.png"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/test4.png"))
 img_list = [my_img1,my_img2,my_img3,my_img4]
+image_number = 0
 
 #Functions
-def forward(image_number):
-	global my_label
-	global button_advance
+def Button_Commands ():
 	global button_previous
+	global button_advance
+
+	if image_number == 0:
+		button_previous = Button (root, text='<', state=DISABLED)
+	else:
+		button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
+	if image_number == len(img_list)-1:
+		button_advance = Button (root, text='>', state=DISABLED)
+	else:
+		button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
+
+def forward(image_number_in):
+	global my_label
+	global Button_Commands
+	global image_number
+	image_number = image_number_in % len(img_list)
 
 	my_label.grid_forget()
-	my_label = Label(image=img_list[image_number-1])
+	my_label = Label(image=img_list[image_number])
 	# button_advance = Button(root,text='>',command = lambda:forward((image_number+1) % len(img_list) ) )
 	# button_previous = Button(root,text='<',command = lambda:back((image_number-1) % len(img_list)))
-	button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
-	button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
-	if image_number == len(img_list):#len(img_list):
-		button_advance = Button (root, text='>', state=DISABLED)
+	# button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
+	# button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
+	# if image_number == len(img_list):#len(img_list):
+	# 	button_advance = Button (root, text='>', state=DISABLED)
 	# else:
 	# 	button_advance = Button(root,text='>',command = lambda:forward((image_number+1) % len(img_list) ) )
+	Button_Commands()
 	my_label.grid(row=2, column=1,columnspan = 3)
 	button_previous.grid(row=1,column=0)
 	button_advance.grid(row=1,column=2)
 
 	return
 
-def back(image_number):
+def back(image_number_in):
 	global my_label
-	global button_advance
-	global button_previous
+	global Button_Commands
+	global image_number
+	image_number = image_number_in  % len(img_list)
 
 	my_label.grid_forget()
-	my_label = Label(image=img_list[image_number-1])
+	my_label = Label(image=img_list[image_number])
+	##THIS SET LOOPS THROUGH LIST
 	# button_advance = Button(root,text='>',command = lambda:forward((image_number+1) % len(img_list) ) )
 	# button_previous = Button(root,text='<',command = lambda:back((image_number-1) % len(img_list)))
-	button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
-	button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
-	if image_number == 1:#len(img_list):
-		button_previous = Button (root, text='<', state=DISABLED)
+	##THIS STOPS YOU GOING BEYOND THE END
+	# button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
+	# button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
+	# if image_number == 1:#len(img_list):
+	# 	button_previous = Button (root, text='<', state=DISABLED)
+	Button_Commands()
+
+
 	my_label.grid(row=2, column=1,columnspan = 3)
 	button_previous.grid(row=1,column=0)
 	button_advance.grid(row=1,column=2)
-
 	return	
+
 
 
 #Create_Widgets
 my_label = Label(image=my_img1)
 button_quit = Button(root, text='Exit Program', command = root.quit)
-button_advance = Button(root,text='>',command = lambda:forward(2) )
-button_previous = Button(root,text='<',command = lambda:back(),state=DISABLED)
-
+Button_Commands()
+# button_advance = Button(root,text='>',command = lambda:forward(2) )
+# button_previous = Button(root,text='<',command = lambda:back(),state=DISABLED)
+# button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
+# button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
+# if image_number == 0:
+# 	button_previous = Button (root, text='<', state=DISABLED)
+# else:
+# 	button_previous = Button(root,text='<',command = lambda:back((image_number-1)))
+# if image_number == len(img_list):
+# 	button_advance = Button (root, text='>', state=DISABLED)
+# else:
+# 	button_advance = Button(root,text='>',command = lambda:forward((image_number+1)))
 
 #Position Widgets
 my_label.grid(row=2, column=1,columnspan = 3)
